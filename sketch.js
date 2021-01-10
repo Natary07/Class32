044ddb260d492304d7fa9aabd48fb5f3d62895af
Natary07 <aryanpat2007@gmail.com>
2021-01-10 11:53:29 -0600

    background

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var bg;
 var gameState = "onSling";
+var score=0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   // backgroundImg = loadImage("sprites/bg.png");
+   getBackgroundImage();
 }
 
 function setup(){
@@ -45,18 +47,25 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    textSize(35);
+    fill("white")
+    text ("score: "+score,980,70);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -67,6 +76,7 @@ function draw(){
     platform.display();
     //log6.display();
     slingshot.display();    
+    //getTime();
 }
 
 function mouseDragged(){
@@ -85,4 +95,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImage() {
+    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo")
+    var responseJSON=await response.json();
+    var datetime=responseJSON.datetime
+    var hour=datetime.slice(11,13);
+    if(hour>=06 && hour<19){
+        bg="sprites/bg.png"
+    }
+    else{
+        bg="sprites/bg2.jpg"
+    }
+    backgroundImg=loadImage(bg)
+    console.log(responseJSON.datetime);
 }
